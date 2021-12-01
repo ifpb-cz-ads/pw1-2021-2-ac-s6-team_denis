074ac92a331f67f1b84c7e4fe3b1a7db6649fee2
Dududenis <dudusjp3@hotmail.com>
2021-12-01 11:14:37 -0300

    Adiciona questao 02 e 03

---
+++ questao_02.py
@@ -0,0 +1,8 @@
+#2) Faça um programa para escrever a contagem regressiva do lançamento de um foguete.
+# O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela
+
+k = 10
+while k >=0:
+    print(k)
+    k = k-1
+print("Fogo!")
